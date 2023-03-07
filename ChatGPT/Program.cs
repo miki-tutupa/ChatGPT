4d4897f59a67bcfcf11d3f59bf59f2694d525f14
Miki Corral <miki@shopworld.es>
2023-03-08 00:40:38 +0100

    ChatGPT console app

--- ChatGPT/Program.cs
+++ ChatGPT/Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using OpenAI.GPT3.Managers;
+using OpenAI.GPT3;
+using Microsoft.Extensions.DependencyInjection;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+using OpenAI.GPT3.ObjectModels;
+using ChatGPT;
+
+#region Configuration
+var builder = Host.CreateDefaultBuilder();
+builder.ConfigureHostConfiguration(icb => { icb.AddUserSecrets<Program>(); });
+var host = builder.Build();
+var configuration = host.Services.GetRequiredService<IConfiguration>();
+var apiKey = configuration.GetSection("OpenAIServiceOptions")["ApiKey"];
+var openAiService = new OpenAIService(new OpenAiOptions() { ApiKey = apiKey! });
+#endregion
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Bienvenido a la consola de ChatGPT, para terminar escribe 'Salir'.");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.DarkGray;
+Console.WriteLine("¿Cómo te llamas? ");
+Console.ForegroundColor = ConsoleColor.White;
+string name = Console.ReadLine()!;
+Console.WriteLine();
+
+string prompt = $"Hola, me llamo {name}";
+
+
+var messages = new List<ChatMessage>
+{
+    //// INFO: FromSystem (developer), FromAssistance (AI), FromUser (user)
+    //ChatMessage.FromSystem($"El usuario se llama {name}."),
+};
+
+do
+{
+    if (prompt != null)
+    {
+        messages.Add(ChatMessage.FromUser(prompt));
+        var completionResult = await openAiService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
+        {
+            Messages = messages,
+            Model = Models.ChatGpt3_5Turbo,
+            MaxTokens = 250,
+            Temperature = (float?)0.8
+        });
+
+        if (completionResult.Successful)
+        {
+            var response = completionResult.Choices.First().Message.Content;
+            if (response[..4] == @"\n\n") response = response[4..];
+            messages.Add(ChatMessage.FromAssistance(response));
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("-ChatGPT dice: ");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            TextService.LineWritter(response);
+            Console.WriteLine();
+        }
+        else
+        {
+            if (completionResult.Error == null)
+            {
+                throw new Exception("Unknown Error");
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"ERROR! {completionResult.Error.Code}: {completionResult.Error.Message}");
+            Console.WriteLine();
+        }
+    }
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine($"-{name ??= "Anónimo"} dice: ");
+    Console.ForegroundColor = ConsoleColor.White;
+    prompt = Console.ReadLine()!;
+    Console.WriteLine();
+
+}
+while (prompt.ToLower() != "salir");
+Console.ResetColor();
+
+
---
+++ ChatGPT/TextService.cs
@@ -0,0 +1,43 @@
+﻿namespace ChatGPT
+{
+    public class TextService
+    {
+        public static void LineWritter(string text)
+        {
+            Random random = new();
+            text = text.Replace("\n", " \n ");
+            int width = Console.WindowWidth;
+            string[] words = text.Split(" ");
+            string line = "";
+
+            foreach (string word in words)
+            {
+                if (word == "\n")
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        Thread.Sleep(random.Next(5, 51));
+                        Console.Write(line[i]);
+                    }
+                    Console.WriteLine();
+                    line = "";
+                }
+                else if (line.Length + word.Length + 1 <= width)
+                {
+                    line += word + " ";
+                }
+                else
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        Thread.Sleep(random.Next(5, 51));
+                        Console.Write(line[i]);
+                    }
+                    Console.WriteLine();
+                    line = word + " ";
+                }
+            }
+            Console.WriteLine(line);
+        }
+    }
+}
